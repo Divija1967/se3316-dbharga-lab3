88298242a8759a9adfe21c9b1e97702498aade6b
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2022-11-01 23:57:36 -0400

    fixed formatting of search results, of genres

--- static/app.js
+++ static/app.js
@@ -4,6 +4,7 @@ document.getElementById('artist-name-btn').addEventListener('click', getArtistLi
 // document.getElementById('album-btn').addEventListener('click', getAlbums);
 document.getElementById('track-btn').addEventListener('click', getTrackList);
 document.getElementById('track-id-btn').addEventListener('click', trackDetails);
+document.getElementById('reset-btn').addEventListener('click', reset);
 const n = 10;
 // use input from user
 
@@ -38,7 +39,20 @@ function artistDetails(){
         header.append(document.createTextNode(`Artist results for: ${artInput.value}`))
         results.forEach(e => {
             const item = document.createElement('li')
-            item.appendChild(document.createTextNode(`${e.artist_id}. ${e.artist_name} - ${e.artist_images} name: ${e.artist_name} About: ${e.artist_bio} Members: ${e.artist_members} Website: ${e.artist_website} Date created: ${e.artist_date_created}`));
+            const h2 = document.createElement('h2');
+            h2.appendChild(document.createTextNode(`${e.artist_id}. Name: ${e.artist_name} `));
+            item.appendChild(h2);
+            item.appendChild(document.createTextNode(`Contact: ${e.artist_handle} \n`));
+            item.appendChild(document.createTextNode(`Members: ${e.artist_members}\n`));
+            var a = document.createElement('a');
+            a.href = (`${e.artist_website}`);
+            a.title = (`${e.artist_website}`);
+            a.append(document.createTextNode(`${e.artist_website}\n`));
+            item.appendChild(document.createTextNode(`Website: `));
+            item.appendChild(a);
+            
+
+            item.appendChild(document.createTextNode(`Date created: ${e.artist_date_created}\n`));
             item.classList.add("box")
             list.appendChild(item);
             
@@ -101,6 +115,7 @@ function trackDetails(){
           // if the input matches an artist name exactly, move it to the top of the array
           if (value === traInput.value) {
             results[i] = data[i];    
+            break;
           } 
         }       
         if(results.length == 0){
@@ -108,9 +123,33 @@ function trackDetails(){
         }else
         header.append(document.createTextNode(`Track result for id number ${traInput.value}`))
         results.forEach(e => {
-            console.log(e);
+            var track_g = e.track_genres;
+            console.log(track_g);
+            var quoted = track_g.replace(/'/g, '"');
+            var parsed = JSON.parse(quoted);
+            var g_string = "";
+
+            for (let i = 0; i < parsed.length; i++){
+                g_string += parsed[i].genre_title;
+                if(i < parsed.length-1){
+                    g_string += ', ';
+                }
+            }
+
             const item = document.createElement('li')
-            item.appendChild(document.createTextNode(`${e.track_id}. ${e.album_id}  ${e.album_title} : ${e.artist_id} : ${e.artist_name} : ${e.tags} : ${e.track_date_created} : ${e.track_date_recorded} : ${e.track_duration} : ${e.track_genres} : ${e.track_number} : ${e.track_title}`));
+            const h3 = document.createElement('h3');
+            h3.appendChild(document.createTextNode(`${e.track_number}.${e.track_title}`));
+            item.appendChild(h3);
+            item.appendChild(document.createTextNode(`Album: ${e.album_id}. ${e.album_title}\n`));
+            item.appendChild(document.createTextNode(`Artist: ${e.artist_id}. ${e.artist_name}\n`));
+            item.appendChild(document.createTextNode(`Tags: ${e.tags.replace(/[\[\]']+/g,'')}\n`));   
+            item.appendChild(document.createTextNode(`Track date created: ${e.track_date_created} \nTrack date recorded: ${e.track_date_recorded} \n`));
+            item.appendChild(document.createTextNode(`Track length: ${e.track_duration}\n`));
+            item.appendChild(document.createTextNode(`Genres: ${g_string}\n`));         
+            
+            
+            
+            
             item.classList.add("box")
             list.appendChild(item);
             
@@ -128,6 +167,9 @@ function getTrackList(){
     let counter = 0;
     clear(list);
     clear(header);
+    if((traInput.value === "")){
+        header.append(document.createTextNode(`Please enter a search value`))
+    }else
     fetch('/api/tracks')
     .then(res => res.json())
     .then(data => {
@@ -170,45 +212,7 @@ function getGenres(){
     .then(data => {
         data.forEach(e => {
             const item = document.createElement('li')
-            item.appendChild(document.createTextNode(`${e.genre_id}. Genre: ${e.title} Genre parent ID: ${e.parent}`));
-            item.classList.add("box")
-            list.appendChild(item);
-        });
-    })
-}
-
-// function getArtists(){
-//     const list = document.getElementById('genre-list');
-//     const genre_div = document.getElementById('genre')
-//     genre_div.classList.toggle('hide');
-//     if(genre_div.classList){
-//         clear(list);
-//     }
-//     fetch('/api/artists')
-//     .then(res => res.json())
-//     .then(data => {
-//         data.forEach(e => {
-//             const item = document.createElement('li')
-//             item.appendChild(document.createTextNode(`${e.artist_id}. Artists:: ${e.artist_name}`));
-//             item.classList.add("box")
-//             list.appendChild(item);
-//         });
-//     })
-// }
-
-function getTracks(){
-    const list = document.getElementById('genre-list');
-    const genre_div = document.getElementById('genre')
-    genre_div.classList.toggle('hide');
-    if(genre_div.classList){
-        clear(list);
-    }
-    fetch('/api/tracks')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(e => {
-            const item = document.createElement('li')
-            item.appendChild(document.createTextNode(`${e.track_id}. Track: ${e.track_title}`));
+            item.appendChild(document.createTextNode(`${e.genre_id}. Genre: ${e.title} - Parent ID: ${e.parent}`));
             item.classList.add("box")
             list.appendChild(item);
         });
@@ -238,4 +242,23 @@ function clear(element){
     while(element.firstChild){
       element.removeChild(element.firstChild);
     }
-  }
+}
+
+function reset(){
+    // from artist search
+    const artList = document.getElementById('artist-list');
+    const artHeader = document.getElementById('h-art');
+    // from track list
+    const trackList = document.getElementById('track-list');
+    const trackHeader = document.getElementById('h-track');
+
+    clear(trackList);
+    clear(trackHeader);
+
+
+    clear(artList);
+    clear(artHeader);
+
+
+
+}
